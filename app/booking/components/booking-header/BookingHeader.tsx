b338e63b2df156524b01c808f58ac6757fe3013e
Breimerct98 <breimer.correa@zabud.com.co>
2023-11-22 23:36:05 -0500

    Update booking header font size

--- app/booking/components/booking-header/BookingHeader.tsx
+++ app/booking/components/booking-header/BookingHeader.tsx
@@ -14,7 +14,7 @@ const BookingHeader: FC = () => {
             <Header className="w-full relative z-20 bg-transparent text-white" />
 
             <div className="absolute left-0 top-0 w-full h-full z-10 grid place-content-center">
-                <h1 className="text-2xl md:text-5xl lg:text-7xl font-bold text-white drop-shadow-2xl relative after:content-['Reservas'] after:absolute after:top-[3px] after:left-0 after:text-black/30 shadow-inner shadow--black">
+                <h1 className="text-7xl font-bold text-white drop-shadow-2xl relative after:content-['Reservas'] after:absolute after:top-[3px] after:left-0 after:text-black/30 shadow-inner shadow--black">
                     Reservas
                 </h1>
             </div>
