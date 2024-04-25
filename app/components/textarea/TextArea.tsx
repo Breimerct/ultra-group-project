8549bb7b0e17dfa0b2a32de4122e2ef590b7fc60
Breimerct98 <breimer.correa@zabud.com.co>
2024-04-25 12:25:20 -0500

    add row prop and format code

--- app/components/textarea/TextArea.tsx
+++ app/components/textarea/TextArea.tsx
@@ -5,21 +5,29 @@ interface IProps extends InputHTMLAttributes<HTMLTextAreaElement> {
     label: string;
     isInvalid?: boolean;
     messageError?: string;
+    rows?: number;
 }
 
-const TextArea: FC<IProps> = ({ label, isInvalid, messageError, ...props }) => {
-    const classError = isInvalid ? "border-red-500 text-red-500 placeholder:text-red-500" : "border-zinc-400";
+const TextArea: FC<IProps> = ({ label, isInvalid, messageError, rows, ...props }) => {
+    const classError = isInvalid
+        ? "border-red-500 text-red-500 placeholder:text-red-500"
+        : "border-zinc-400";
     const classlabelError = isInvalid ? "text-red-500" : null;
     const classLabelReadOnly = props.readOnly ? "text-zinc-600/70" : null;
 
     return (
         <>
-            <label className={`block ${classlabelError} ${classLabelReadOnly}`}>{label}</label>
+            <label className={`block ${classlabelError} ${classLabelReadOnly}`}>
+                {label}
+            </label>
             <textarea
                 {...props}
+                rows={rows}
                 className={`outline-none rounded-md border-2 border-solid p-2 w-full read-only:border-zinc-500/20 read-only:text-zinc-600/70 read-only:pointer-events-none resize-none ${classError}`}
             />
-            {isInvalid && <span className="text-red-500 text-xs ml-2">{messageError}</span>}
+            {isInvalid && (
+                <span className="text-red-500 text-xs ml-2">{messageError}</span>
+            )}
         </>
     );
 };
